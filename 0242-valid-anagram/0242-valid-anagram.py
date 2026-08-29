53706247dc8de66ad01505ba1f03ec9826e415ba
Paramaguru432 <paramaguru432@gmail.com>
2026-08-29 20:34:53 +0530

    Time: 11 ms (96.09%), Space: 12.4 MB (98.66%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        freq1 = {}
+        freq2 = {}
+
+        for ch in s:
+            if ch in freq1:
+                freq1[ch] = freq1[ch] + 1
+            else:
+                freq1[ch] = 1
+
+        for ch in t:
+            if ch in freq2:
+                freq2[ch] = freq2[ch] + 1
+            else:
+                freq2[ch] = 1
+
+        if freq1 == freq2:
+            return True
+        else:
+            return False
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
