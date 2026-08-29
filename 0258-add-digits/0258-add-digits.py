752a6e1e3aca9560331dff4b57c6d80fb87b5155
Paramaguru432 <paramaguru432@gmail.com>
2026-08-29 21:22:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.93%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+
+            total = 0
+
+            while num > 0:
+                total += num % 10
+                num //= 10
+
+            num = total
+
+        return num
+        """
+        :type num: int
+        :rtype: int
+        """
+        
