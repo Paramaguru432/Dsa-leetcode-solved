c54d24a32cd02dbd76081990d11bcd72cc2096d6
Paramaguru432 <paramaguru432@gmail.com>
2026-08-29 18:24:19 +0530

    Time: 7 ms (36.34%), Space: 15.2 MB (5.66%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,17 @@
 class Solution(object):
     def singleNumber(self, nums):
-        result=0
-        for num in nums:
-            result^=num
-        return result    
         """
         :type nums: List[int]
         :rtype: int
         """
-        
+        if nums == 1:
+            return 1
+        dict = {}
+        for i in nums:
+            if i in dict:
+                dict[i] +=1
+            else :
+                dict[i] = 0
+        for key,value in dict.items():
+            if value==0:
+                return key
