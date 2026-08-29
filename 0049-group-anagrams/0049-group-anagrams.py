b5819c5e9987027b6e54813752dd6fd1cc762f37
Paramaguru432 <paramaguru432@gmail.com>
2026-08-29 20:53:38 +0530

    Time: 24 ms (35.12%), Space: 16.2 MB (41.76%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = {}
+
+        for word in strs:
+            key = ''.join(sorted(word))
+
+            if key not in groups:
+                groups[key] = []
+
+            groups[key].append(word)
+
+        return list(groups.values())
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
