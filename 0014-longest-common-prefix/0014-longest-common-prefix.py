caceca53010fdb06083686b21aa533883d0db986
Paramaguru432 <paramaguru432@gmail.com>
2026-08-29 20:54:46 +0530

    Time: 0 ms (100%), Space: 12.5 MB (34.32%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        reference=strs[0]
+        for char_index in range(len(reference)):
+            current_char=reference[char_index]
+            for word in strs[1:]:
+                if char_index==len(word) or word[char_index]!=current_char:
+                    return reference[:char_index]
+        return reference            
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
