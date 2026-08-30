6c911709bf1b55f06dedd15e3e10a519f0580840
Paramaguru432 <paramaguru432@gmail.com>
2026-08-31 00:16:00 +0530

    Time: 8 ms (7.14%), Space: 19.5 MB (42.55%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,63 +1,28 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        # Always binary search on the smaller array
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:      
+        i = j = 0
+        result = []
 
-        m = len(nums1)
-        n = len(nums2)
-
-        left = 0
-        right = m
-
-        while left <= right:
-
-            partition1 = (left + right) // 2
-            partition2 = (m + n + 1) // 2 - partition1
-
-            # Left and right values around partition
-            if partition1 == 0:
-                left1 = float('-inf')
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                result.append(nums1[i])
+                i += 1
             else:
-                left1 = nums1[partition1 - 1]
+                result.append(nums2[j])
+                j += 1
 
-            if partition1 == m:
-                right1 = float('inf')
-            else:
-                right1 = nums1[partition1]
+        while i < len(nums1):
+            result.append(nums1[i])
+            i += 1
 
-            if partition2 == 0:
-                left2 = float('-inf')
-            else:
-                left2 = nums2[partition2 - 1]
+        while j < len(nums2):
+            result.append(nums2[j])
+            j += 1
+            
+        n = len(result)
 
-            if partition2 == n:
-                right2 = float('inf')
-            else:
-                right2 = nums2[partition2]
-
-            # Correct partition
-            if left1 <= right2 and left2 <= right1:
-
-                max_left = max(left1, left2)
-
-                if (m + n) % 2 == 1:
-                    return float(max_left)
-
-                min_right = min(right1, right2)
-
-                return (max_left + min_right) / 2.0
-
-            # partition1 is too far right
-            elif left1 > right2:
-                right = partition1 - 1
-
-            # partition1 is too far left
-            else:
-                left = partition1 + 1
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
+        if n % 2 == 1:          # Odd length
+            return float(result[n // 2])
+        else:                   # Even length
+            return float((result[n // 2 - 1] + result[n // 2]) / 2)
         
