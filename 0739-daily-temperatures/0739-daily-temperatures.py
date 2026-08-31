6f0d2bb19d5b9066ce7d836f8d2edfa8850d98b8
Paramaguru432 <paramaguru432@gmail.com>
2026-08-31 17:57:58 +0530

    Time: 179 ms (33.08%), Space: 24.5 MB (89.64%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+
+        answer = [0] * len(temperatures)
+
+        stack = []
+
+        i = 0
+
+        while i < len(temperatures):
+
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+
+                previous = stack.pop()
+
+                answer[previous] = i - previous
+
+            stack.append(i)
+
+            i += 1
+        return answer
+
+                
