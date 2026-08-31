eefb89a5a0f362cb7b835d5cbe6ec9f2b867a57f
Paramaguru432 <paramaguru432@gmail.com>
2026-08-31 14:33:37 +0530

    Time: 8 ms (68.73%), Space: 23.1 MB (29.19%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        import heapq
+
+        dummy = ListNode(0)
+        tail = dummy
+
+        heap = []
+
+        # Put first node of every list into heap
+        for i in range(len(lists)):
+
+            if lists[i] is not None:
+
+                heapq.heappush(
+                    heap,
+                    (lists[i].val, i, lists[i])
+                )
+
+        # Continue until heap becomes empty
+        while heap:
+
+            # Get smallest node
+            value, i, node = heapq.heappop(heap)
+
+            # Attach node to result
+            tail.next = node
+            tail = tail.next
+
+            # Put next node into heap
+            if node.next is not None:
+
+                heapq.heappush(
+                    heap,
+                    (node.next.val, i, node.next)
+                )
+
+        return dummy.next
