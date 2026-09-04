e155a2de6ea58830c8640e81d2fc7d600336877f
Paramaguru432 <paramaguru432@gmail.com>
2026-09-04 12:20:19 +0530

    Time: 0 ms (100%), Space: 12.5 MB (95.56%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        def check(left, right):
+
+            if left == None and right == None:
+                return True
+
+            if left == None or right == None:
+                return False
+
+            if left.val != right.val:
+                return False
+
+            return check(left.left, right.right) and check(left.right, right.left)
+
+        return check(root.left, root.right)
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        
