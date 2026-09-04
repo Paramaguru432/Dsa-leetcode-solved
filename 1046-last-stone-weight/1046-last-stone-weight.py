d7eacc592402a7eaa1407852116043f2ba664f69
Paramaguru432 <paramaguru432@gmail.com>
2026-09-04 11:44:34 +0530

    Time: 4 ms (12.32%), Space: 12.5 MB (17.16%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        import heapq
+       
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = -heapq.heappop(stones)
+            second = -heapq.heappop(stones)
+
+            if first != second:
+                heapq.heappush(stones, -(first - second))
+
+        if stones:
+            return -stones[0]
+        else:
+            return 0
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        
