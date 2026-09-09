db9ac475a6fb3902eca1b872457e0b8c5324b39b
Paramaguru432 <paramaguru432@gmail.com>
2026-09-09 09:27:06 +0530

    Time: 0 ms (100%), Space: 12.9 MB (99.12%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        # Build graph
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+            indegree[course] += 1
+
+        queue = deque()
+
+        # Courses with no prerequisites
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        result = []
+
+        while queue:
+            course = queue.popleft()
+            result.append(course)
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        # Cycle exists
+        if len(result) != numCourses:
+            return []
+
+        return result
+        
