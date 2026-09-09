eb189e78d6f050669861b7bac64f565d43870210
Paramaguru432 <paramaguru432@gmail.com>
2026-09-09 19:21:13 +0530

    Time: 95 ms (18.74%), Space: 15.8 MB (7.28%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        m = len(word1)
+        n = len(word2)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # If word2 is empty
+        for i in range(m + 1):
+            dp[i][0] = i
+
+        # If word1 is empty
+        for j in range(n + 1):
+            dp[0][j] = j
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+
+                else:
+                    insert = dp[i][j - 1]
+                    delete = dp[i - 1][j]
+                    replace = dp[i - 1][j - 1]
+
+                    dp[i][j] = 1 + min(
+                        insert,
+                        delete,
+                        replace
+                    )
+
+        return dp[m][n]
